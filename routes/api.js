12f698d56a134cfd2b126a1f0ced512429510460
Adrian Ko <adrianchko@gmail.com>
2014-07-03 12:03:13 +0100

    Fix indent

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({a: 1}));
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({a: 1}));
 });
 
 module.exports = router;
